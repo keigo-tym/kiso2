fdf4a29bf61f3283e75fb7e6c8c6129001d08bc2
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-04 11:45:43 +0900

    Update SimpleCalc.php

--- section3/SimpleCalc.php
+++ section3/SimpleCalc.php
@@ -2,4 +2,14 @@
 class SimpleCalc
 {
     public $number;
+
+    public function add($x)
+    {
+        $this->number = $this->number + $x;
+    }
+
+    public function show()
+    {
+        echo $this->number . PHP_EOL;
+    }
 }
