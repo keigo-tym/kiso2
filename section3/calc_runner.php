a424364c149a1fc560ec30fb09e37873c36aacf3
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-04 11:45:40 +0900

    Update calc_runner.php

--- section3/calc_runner.php
+++ section3/calc_runner.php
@@ -2,13 +2,9 @@
 require_once("SimpleCalc.php");
 
 $calc = new SimpleCalc();
-$calc2 = new SimpleCalc();
 
 $calc->number = 10;
-$calc2->number = 20;
 
-echo $calc->number . PHP_EOL;
-echo $calc2->number . PHP_EOL;
-
-var_dump($calc);
-var_dump($calc2);
+$calc->add(20);
+$calc->add(30);
+$calc->show();
