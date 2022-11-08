bd16331e429e31901f7df8165aa5d45dc8495854
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-08 10:14:45 +0900

    Update my_class_runner.php

--- section4/my_class_runner.php
+++ section4/my_class_runner.php
@@ -4,7 +4,7 @@ require_once("MyClass.php");
 echo "1" . PHP_EOL;
 try {
     $myClass = new MyClass("Hello");
-    $myClass->myMethod("");
+    $myClass->myMethod("Andy");
     echo "2" . PHP_EOL;
 } catch (Exception $e) {
     echo "Catch exception." . PHP_EOL;
