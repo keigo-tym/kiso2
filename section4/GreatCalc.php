890327e37e2b60d66fad7e1074e6ab669d2bd61b
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-09 11:20:20 +0900

    Update GreatCalc.php

--- section4/GreatCalc.php
+++ section4/GreatCalc.php
@@ -1,9 +1,18 @@
 <?php
 require_once("SimpleCalc.php");
 
-class GreatCalc extends SimpleCalc {
+class GreatCalc extends SimpleCalc 
+{
+    protected $message = "Answer: ";
+    
     public function pow($x = 2)
     {
         $this->number = $this->number ** $x;
+    }
+
+    public function show()
+    {
+        echo $this->message;
+        echo $this->number . PHP_EOL;
     }
 }
