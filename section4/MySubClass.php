ae017e39fc6675133caf18771fe67d0f793ce7c6
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-09 11:37:16 +0900

    Update MySubClass.php

--- section4/MySubClass.php
+++ section4/MySubClass.php
@@ -1,11 +1,16 @@
 <?php
 require_once("MyClass.php");
+require_once("MyException.php");
 
 class MySubClass extends MyClass
-{
-    
+{ 
     public function myMethod($x)
     {
+        if ($x === "") {
+            $e = new MyException('$x is empty.');
+            throw $e;
+        }
+
         echo "Overreide!" . $this->myProperty . " " . $x . PHP_EOL;
     }
     
