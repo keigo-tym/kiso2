563ce9931e9459855b57cb6588e26849d3dd5e98
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-09 11:37:08 +0900

    Update my_class_runner.php

--- section4/my_class_runner.php
+++ section4/my_class_runner.php
@@ -1,6 +1,11 @@
 <?php
 require_once("MySubClass.php");
 
-$mySubClass = new MySubClass("Hello");
-$mySubClass->myMethod("Karen");
-$mySubClass->myMethod2();
+try {
+    $mySubClass = new MySubClass("Hello");
+    $mySubClass->myMethod("");
+    $mySubClass->myMethod2();
+} catch(MyException $e) {
+    echo "Catch exception." . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+}
