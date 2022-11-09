1a71a60aaf747b8bdd22ed1b3ae7921bc03c8c79
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-09 11:15:21 +0900

    Update MySubClass.php

--- section4/MySubClass.php
+++ section4/MySubClass.php
@@ -3,6 +3,12 @@ require_once("MyClass.php");
 
 class MySubClass extends MyClass
 {
+    
+    public function myMethod($x)
+    {
+        echo "Overreide!" . $this->myProperty . " " . $x . PHP_EOL;
+    }
+    
     public function myMethod2()
     {
         echo "Hello World" . PHP_EOL;
