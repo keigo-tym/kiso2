18fd16b868f41748a8212ce5bab437398451c852
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-10 09:59:05 +0900

    Update my_class_runner.php

--- section4/my_class_runner.php
+++ section4/my_class_runner.php
@@ -1,11 +1,5 @@
 <?php
-require_once("MySubClass.php");
+require_once("MyClass.php");
 
-try {
-    $mySubClass = new MySubClass("Hello");
-    $mySubClass->myMethod("");
-    $mySubClass->myMethod2();
-} catch(MyException $e) {
-    echo "Catch exception." . PHP_EOL;
-    echo $e->getMessage() . PHP_EOL;
-}
+MyClass::$myStaticProperty = "Hello";
+MyClass::myStaticMethod("World");
