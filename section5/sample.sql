420f43e43e248cc6f63e2b1e61cad80b35e64c26
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-25 10:41:28 +0900

    Update sample.sql

--- section5/sample.sql
+++ section5/sample.sql
@@ -10,3 +10,8 @@ select * from categories;
 update categories set title = 'PG' where id = 1;
 
 delete from categories where id = 1;
+
+create table categories(
+    id integer primary key,
+    title text
+);
