5d1e41f19d260d5edac8a810cabe594aa29dd7f0
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-29 17:37:08 +0900

    Update courses.sql

--- section5/courses.sql
+++ section5/courses.sql
@@ -13,3 +13,5 @@ insert into courses (id, title, learning_time, category_id) values (4, 'Web Desi
 select * from courses;
 select id, title, learning_time, category_id from courses where id = 4;
 select * from courses where id = 4;
+
+update courses set learning_time = 100 where id = 4;
