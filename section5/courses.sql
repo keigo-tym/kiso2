e9c93fef37b131b9c78ef9fd2c528737f1d4ab38
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-11-29 17:27:18 +0900

    Update courses.sql

--- section5/courses.sql
+++ section5/courses.sql
@@ -4,3 +4,8 @@ create table courses(
     learning_time integer,
     category_id integer
 );
+
+insert into courses (id, title, learning_time, category_id) values (1, 'PHP Basic', 30, 1);
+insert into courses (id, title, learning_time, category_id) values (2, 'PHP Database', 20, 1);
+insert into courses (id, title, learning_time, category_id) values (3, 'Python Basic', 30, 1);
+insert into courses (id, title, learning_time, category_id) values (4, 'Web Design', 50, 2);
