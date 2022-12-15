f56897d09017ab5f690e63ec20054ceeb6a1cd01
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-12-15 15:25:39 +0900

    Update training_01.sql

--- section6/training_01.sql
+++ section6/training_01.sql
@@ -3,3 +3,5 @@ select title from categories where id = 2;
 select * from categories where title like '%ing';
 
 select * from categories order by id desc;
+
+select upper(title) as u_title from categories;
