72fb19be489a29369c918249d7d56f72ed220ad7
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2022-12-15 15:31:22 +0900

    Update training_01.sql

--- section6/training_01.sql
+++ section6/training_01.sql
@@ -5,6 +5,7 @@ select * from categories where title like '%ing';
 select * from categories order by id desc;
 
 select upper(title) as u_title from categories;
+select upper(title) u_title from categories;
 
 select count(title) category_count from categories;
 select count(*) category_count from categories;
