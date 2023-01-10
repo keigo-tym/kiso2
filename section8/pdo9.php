233700a99f0f2a1990cdcc678290c7b0d43a2a39
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-10 11:02:30 +0900

    Create pdo9.php

---
+++ section8/pdo9.php
@@ -0,0 +1,16 @@
+<?php
+try {
+    $dsn = "sqlite:eldb.sqlite3";
+    $pdo = new PDO($dsn);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "select id, title from categori";
+    $st = $pdo->query($sql);
+    var_dump($st);
+
+} catch (PDOException $e) {
+    echo "catch" . PHP_EOL;
+    echo $e->getCode() . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+}
+echo "end" . PHP_EOL;
