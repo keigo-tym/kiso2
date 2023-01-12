3ef0bbf159694e40b1d8bac2bcbc78a8af44b810
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-12 19:12:03 +0900

    Create pdo13.php

---
+++ section8/pdo13.php
@@ -0,0 +1,19 @@
+<?php
+try  {
+    $dsn = "sqlite:eldb.sqlite3";
+    $pdo = new PDO($dsn);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $sql = "select * from categories where title = ?";
+    $ps = $pdo->prepare($sql);
+
+    $title = "Photo";
+    $ps->bindValue(1, $title, PDO::PARAM_STR);
+    $ps->execute();
+
+    $rows = $ps->fetchAll();
+    var_dump($rows);
+} catch (PDOException $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
