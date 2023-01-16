dfe69cb4438bfdedf7eb4b08e3c0a293fad3aaa6
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-16 10:08:11 +0900

    Update category_seaech.php

--- section8/category_seaech.php
+++ section8/category_seaech.php
@@ -5,8 +5,13 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 $id = filter_input(INPUT_GET, "id");
-$sql = "select id, title from categories where id = " . $id;
-$st = $pdo->query($sql);
+
+// Prepared Statement
+$sql = "select id, title from categories where id = :id";
+$st = $pdo->prepare($sql);
+$st->bindValue(":id", $id, PDO::PARAM_INT);
+$st->execute();
+
 $rows = $st->fetchAll();
 ?>
 <!DOCTYPE html>
