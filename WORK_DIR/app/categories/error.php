5d05f4f041f33ed0141f936ba33985716d5d1c00
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-17 14:31:29 +0900

    Create error.php

---
+++ WORK_DIR/app/categories/error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP DB</title>
+</head>
+<body>
+    <h3>Categories - Error</h3>
+    <hr>
+    <a href="index.php">Back</a>
+</body>
+</html>
