45110c44002885f6bfd1946ffcf424e946a5254b
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-18 10:05:56 +0900

    Update show.php

---
+++ WORK_DIR/app/categories/show.php
@@ -0,0 +1,52 @@
+<?php
+$id = (string)filter_input(INPUT_GET, "id");
+if ($id === "") {
+    error_log("Validate: id is required.");
+    header("Location: error.php");
+    exit();
+}
+if (filter_var($id, FILTER_VALIDATE_INT) === false) {
+    error_log("Validate: id is not int.");
+    header("Location: error.php");
+    exit();
+}
+
+try {
+    $options = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
+    $pdo = new PDO("sqlite:../../db/eldb.sqlite3",null, null, $options);
+
+    $sql = "select id, title from categories where id = :id";
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(":id", $id, PDO::PARAM_INT);
+    $ps->execute();
+    $category = $ps->fetch();
+    if ($category === false) {
+        error_log("Invalid id. $id");
+        header("Location: error.php");
+        exit();
+    }
+} catch (PDOException $e) {
+    error_log("PDOException: " . $e->getMessage());
+    header("Location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP DB</title>
+</head>
+<body>
+    <h3>Categories - Show</h3>
+    <hr>
+    ID: <?= htmlspecialchars($category["id"]) ?><br>
+    TITLE: <?= htmlspecialchars($category["title"]) ?><br>
+    <hr>
+    <a href="index.php">BACK</a>
+</body>
+</html>
