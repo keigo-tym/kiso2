6bea284091686d51b806df2c4b7282c665e9a56a
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-18 11:31:21 +0900

    Update destroy.php

---
+++ WORK_DIR/app/categories/destroy.php
@@ -0,0 +1,32 @@
+<?php
+$id = (string)filter_input(INPUT_POST, "id");
+if ($id === "") {
+    error_log("Validate: id is required.");
+    header("Location: error.php");
+    exit();
+}
+if (filter_var($id, FILTER_VALIDATE_INT) === false) {
+    error_log("Validate: id is not int.");
+    header("Location: error.php");
+    exit();
+}
+
+try {
+    $options = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
+    $pdo = new PDO("sqlite:../../db/eldb.sqlite3",null, null, $options);
+
+    $sql = "delete from categories where id = :id";
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(":id", $id, PDO::PARAM_INT);
+    $ps->execute();
+
+    header("Location: index.php");
+
+} catch (PDOException $e) {
+    error_log("PDOException: " . $e->getMessage());
+    header("Location: error.php");
+}
