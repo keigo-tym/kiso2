83420e6aa4f686d1a94385a82c9bb34e5c04b5d4
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-19 13:43:59 +0900

    Create error.php

---
+++ WORK_DIR/app/courses/error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP DB</title>
+</head>
+<body>
+  <h3>Courses - Error</h3>
+  <hr>
+  <a href="index.php">BACK</a>
+</body>
+</html>
