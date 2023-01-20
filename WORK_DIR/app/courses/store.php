eec588b32fb40b6fb0cbed77673c5ca25fc3af3d
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-20 11:04:38 +0900

    Update store.php

--- WORK_DIR/app/courses/store.php
+++ WORK_DIR/app/courses/store.php
@@ -1,2 +1,22 @@
 <?php
-// TODO
+        
+try {
+
+  $dsn = "sqlite:../../db/eldb.sqlite3";
+  $options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
+  ];
+  $pdo = new PDO($dsn, null, null, $options);
+
+
+  $sql = "insert into courses (id, title, learning_time, category_id) 
+        values (:id, :title, :learning_time, :category_id)";
+
+
+} catch (PDOException $e) {
+        error_log("PDOException: " . $e->getMessage());
+        header("Location: error.php");
+        exit();
+}
