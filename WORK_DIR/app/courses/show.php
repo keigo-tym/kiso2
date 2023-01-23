2e54570871909a577ffabe93fc6d1869d75bb510
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-23 10:14:11 +0900

    Update show.php

--- WORK_DIR/app/courses/show.php
+++ WORK_DIR/app/courses/show.php
@@ -1,5 +1,45 @@
 <?php
 // TODO
+$id = (string)filter_input(INPUT_GET, "id");
+if ($id === "") {
+    error_log("Validate: id is required.");
+    header("Location: error.php");
+    exit();
+}
+if (filter_var($id, FILTER_VALIDATE_INT) === false) {
+    error_log("Validate: id is not int.");
+    header("Location: error.php");
+    exit();
+}
+
+try {
+    $options = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
+    $pdo = new PDO("sqlite:../../db/eldb.sqlite3",null, null, $options);
+
+    $sql = "select
+            co.id, co.title, co.learning_time, ca.title category_title
+            from
+            courses co left join categories ca on category_id = ca.id
+            where
+            co.id = :id";
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(":id", $id, PDO::PARAM_INT);
+    $ps->execute();
+    $course = $ps->fetch();
+    if ($course === false) {
+        error_log("Invalid id. $id");
+        header("Location: error.php");
+        exit();
+    }
+} catch (PDOException $e) {
+    error_log("PDOException: " . $e->getMessage());
+    header("Location: error.php");
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
