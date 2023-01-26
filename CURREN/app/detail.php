fcd3d66f601a6ea98396b4a481c36e17eb259c97
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-26 09:18:07 +0900

    Update detail.php

--- CURREN/app/detail.php
+++ CURREN/app/detail.php
@@ -1,3 +1,82 @@
+<?php
+require_once("../libs/functions.php");
+$course_id = (string)filter_input(INPUT_GET, "course_id");
+if ($course_id === "") {
+    error_log("Validate: course_id is required.");
+    header("Location: error.php");
+    exit();
+}
+if (filter_var($course_id, FILTER_VALIDATE_INT) === false) {
+    error_log("Validate: course_id is not int.");
+    header("Location: error.php");
+    exit();
+}
+
+$section_id = (string)filter_input(INPUT_GET, "section_id");
+if ($section_id !== "" && filter_var($section_id, FILTER_VALIDATE_INT) === false) {
+    error_log("Validate: section_id is not int.");
+    header("Location: error.php");
+    exit();
+}
+
+try {
+    $pdo = new_PDO();
+    $sql = "select 
+                co.id,
+                co.title course_title,
+                ca.title category_title
+            from
+                courses co
+                inner join categories ca on co.category_id = ca.id
+            where
+                co.id = :id";
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(":id", $course_id, PDO::PARAM_INT);
+    $ps->execute();
+    $course = $ps->fetchAll();
+    if ($course === false) {
+        error_log("Invalid course id." . $course_id);
+        header("Location: error.php");
+        exit();
+    }
+
+    $sql = "select
+                se.id,
+                se.title,
+                se.no,
+                se.url,
+                se.course_id
+            from
+                sections se
+            where
+                se.course_id = :course_id
+            order by
+                se.no";
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(":course_id", $course_id, PDO::PARAM_INT);
+    $ps->execute();
+    $sections = $ps->fetchAll();
+    if (count($sections) === 0) {
+        error_log("Invalid sections." . $course_id);
+        header("Location: error.php");
+        exit();
+    }
+
+    $current_section = $sections[0];
+    foreach ($sections as $section) {
+        if ((int)$section["id"] === (int)$section_id) {
+            $current_section = $section;
+            break;
+        }
+    }
+
+} catch(PDOException $e) {
+    error_log($e->getMessage());
+    header("Location: error.php");
+    exit();
+} 
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -38,35 +117,26 @@
         <div class="row">
             <div class="col-md-3 mb-4">
                 <div class="card shadow-sm">
-                    <img src="img/courses/1.png" alt="course image">
+                    <img src="img/courses/<?= h($course["id"]) ?>.png" alt="course image">
                     <div class="card-body">
-                        <h5 class="card-title">PHP Basic</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Programming</h6>
+                        <h5 class="card-title"><?= h($course["course_title"]) ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?= h($course["category_title"]) ?></h6>
                     </div>
                     <ul class="list-group list-group-flush">
+                        <?php foreach ($sections as $section) { ?>
                         <li class="list-group-item">
-                            <a href="detail.php?course_id=1&section_id=1">Section 1 : Variance</a>
+                            <a href="detail.php?course_id=<?= h($course["id"]) ?>&section_id=<?= h($section["id"]) ?>">
+                                Section <?= h($section["no"]) ?> : <?= h($section["title"]) ?></a>
                         </li>
-                        <li class="list-group-item">
-                            <a href="detail.php?course_id=1&section_id=2">Section 2 : Array</a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="detail.php?course_id=1&section_id=3">Section 3 : Loop</a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="detail.php?course_id=1&section_id=4">Section 4 : Condition</a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="detail.php?course_id=1&section_id=5">Section 5 : Function</a>
-                        </li>
+                        <?php } ?>
                     </ul>
                 </div>
             </div>
             <div class="col-md-9 mb-4">
-                <video src="https://d2rl9vvq6hawml.cloudfront.net/phpdb/php-basic-1.mp4" 
+                <video src="<?= h($current_section["url"]) ?>" 
                     playsinline controls class="section-video"></video>
                 <hr>
-                <h5 class="my-4">PHP Basic - Section 1 : Variance</h5>
+                <h5 class="my-4"><?= h($course["course_title"]) ?> - Section <?= h($current_section["no"]) ?> : <?= h($current_section["title"]) ?></h5>
             </div>
         </div>
     </main>
