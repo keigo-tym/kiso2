d467bfe32766165b36aeb144a8480631ca90e672
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-30 10:43:07 +0900

    Update CourseDAO.php

--- CURREN/libs/CourseDAO.php
+++ CURREN/libs/CourseDAO.php
@@ -4,6 +4,22 @@ class CourseDAO
 {
     public function __construct($pdo)
     {
-        
+        $this->pdo = $pdo;
+    }
+
+    public function selectAll()
+    {
+        $sql = "select
+                co.id,
+                co.title course_title,
+                ca.title category_title 
+            from 
+                courses co 
+                inner join categories ca on co.category_id = ca.id 
+            order by 
+                co.id";
+    $st = $this->pdo->query($sql);
+    $courses = $st->fetchAll();
+    return $courses;
     }
 }
