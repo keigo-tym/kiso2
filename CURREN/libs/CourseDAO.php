c5a02d6ab718fd6a271753a1ec070935a3f83c0e
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-01-30 10:53:48 +0900

    Update CourseDAO.php

--- CURREN/libs/CourseDAO.php
+++ CURREN/libs/CourseDAO.php
@@ -18,8 +18,26 @@ class CourseDAO
                 inner join categories ca on co.category_id = ca.id 
             order by 
                 co.id";
-    $st = $this->pdo->query($sql);
-    $courses = $st->fetchAll();
-    return $courses;
+        $st = $this->pdo->query($sql);
+        $courses = $st->fetchAll();
+        return $courses;
+    }
+
+    public function selectById($course_id)
+    {
+        $sql = "select 
+                co.id,
+                co.title course_title,
+                ca.title category_title
+            from
+                courses co
+                inner join categories ca on co.category_id = ca.id
+            where
+                co.id = :id";
+        $ps = $this->pdo->prepare($sql);
+        $ps->bindValue(":id", $course_id, PDO::PARAM_INT);
+        $ps->execute();
+        $course = $ps->fetch();
+        return $course;
     }
 }
