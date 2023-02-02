cb3dee9b7f90991db306217168b946a7be571b87
keigo-toyama <104549980+kei-123456789@users.noreply.github.com>
2023-02-02 14:20:06 +0900

    Update AccountDAO.php

--- CURREN/libs/AccountDAO.php
+++ CURREN/libs/AccountDAO.php
@@ -27,10 +27,10 @@ class AccountDAO
     public function insert($name, $hashed_password)
     {
         $sql = "insert into accounts (name, hashed_password)
-                    value (:name, :hashed_password)";
+                    values (:name, :hashed_password)";
         $ps = $this->pdo->prepare($sql);
         $ps->bindValue(":name", $name, PDO::PARAM_STR);
-        $ps->bindValue(":hashed_password", $hashed_password, PARAM_STR);
+        $ps->bindValue(":hashed_password", $hashed_password, PDO::PARAM_STR);
         $ps->execute();
     }
 }
